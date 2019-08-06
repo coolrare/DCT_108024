6c46b5d3c6ff564c22ec6c921cb26f617412d9c7
Will <doggy.huang@gmail.com>
2019-08-06 14:12:34 +0800

    透過 FormBuilder 建立表單模型 & 註冊 ngSubmit 事件

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     document.body.className = 'bg-gradient-primary';
+
+    this.form = this.fb.group({
+      firstName: ['',
+          [Validators.required]
+      ],
+      lastName: ['',
+          [Validators.required]
+      ],
+      email: ['',
+          [Validators.required, Validators.email]
+      ],
+      pw: ['',
+          [Validators.required, Validators.minLength(3)]
+      ],
+      pw2: ['',
+          [Validators.required, Validators.minLength(3)]
+      ]
+    });
+  }
+
+  doRegister(form: NgForm) {
+    if (this.form.valid) {
+      console.log('送出表單', this.form.value);
+    }
   }
 
 }
