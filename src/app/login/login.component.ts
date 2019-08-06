70293a4ccc53c8d65712aa71d9436bc7dffff7ec
Will <doggy.huang@gmail.com>
2019-08-06 10:39:46 +0800

    示範 errors 屬性如何解決型別問題與使用 ? 運算子

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   data: any = {
-    username: '',
+    username: 'doggy.huang@gmail.com',
     password: '',
     rememberMe: true
   };
